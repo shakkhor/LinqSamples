9e2ca0f52efb000f207f6b9ad8803a8c5872e2e1
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2020-04-21 12:50:35 +0600

    linq and c#

---
+++ Features/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+               new Employee{Id=1, Name="Dudu"},
+               new Employee{Id=2,Name="Hagu"}
+            };
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee{Id=3,Name="BoyalMach"}
+            };
+            Console.WriteLine($"Number of developers: {developers.Count()}");
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
